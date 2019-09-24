5e5ee843b87a546232687b3289aee1b6ed51276d
kudog <kudo.gergely@kulcs-soft.hu>
2019-09-24 10:39:15 +0200

    processes model bugfix

--- src/main/java/com/ks/frontend/model/Processes.java
+++ src/main/java/com/ks/frontend/model/Processes.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Component;
 @Entity
 @Table(name = "processes")
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlRootElement(name = "PROCESSES")
 public class Processes {
 
     @Id
@@ -22,42 +21,33 @@ public class Processes {
     @XmlTransient
     public Integer id;
     @Column(nullable = false, unique = true)
-    @XmlTransient
     public String apiKey;
     @Column(nullable = false, unique = true)
-    @XmlElement(name = "DOWNLOAD_PRODUCT")
     public Integer getProduct;
     @Column(nullable = false, unique = true)
-    @XmlElement(name = "UPLOAD_PRODUCT")
     public Integer setProduct;
     @Column(nullable = false, unique = true)
-    @XmlElement(name = "DOWNLOAD_CUSTOMER")
     public Integer getCustomer;
     @Column(nullable = false, unique = true)
-    @XmlElement(name = "UPLOAD_CUSTOMER") 
     public Integer setCustomer;
     @Column(nullable = false, unique = true)
-    @XmlElement(name = "DOWNLOAD_ORDER")
     public Integer getOrder;
     @Column(nullable = false, unique = true)
-    @XmlElement(name = "UPLOAD_ORDER")
     public Integer setOrder;
     @Column(nullable = false, unique = true)
-    @XmlElement(name = "DOWNLOAD_PRODUCTCATEGORIES")
-    public Integer getProductCategories;
+    public Integer setProductCategories;
     @Column(nullable = false, unique = true)
-    @XmlElement(name = "DOWNLOAD_STOCK")
-    public Integer getStock;
+    public Integer setStock;
 
-    public Processes(int getProduct, int setProduct, int getCustomer, int setCustomer, int getOrder, int setOrder, int getProductCategories, int getStock, String apiKey){
+    public Processes(int getProduct, int setProduct, int getCustomer, int setCustomer, int getOrder, int setOrder, int setProductCategories, int setStock, String apiKey){
         this.getProduct = getProduct;
         this.setProduct = setProduct;
         this.getCustomer = getCustomer;
         this.setCustomer = setCustomer;
         this.getOrder = getOrder;
         this.setOrder = setOrder;
-        this.getProductCategories = getProductCategories;
-        this.getStock = getStock;
+        this.setProductCategories = setProductCategories;
+        this.setStock = setStock;
         this.apiKey = apiKey;
     }
 
